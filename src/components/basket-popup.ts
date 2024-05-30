be1671a268a0f8ba2ce63cdaaa1a6c2163e85751
Alina <alinbrode@yandex.ru>
2024-05-30 13:37:21 +0300

    basket button

--- src/components/basket-popup.ts
+++ src/components/basket-popup.ts
@@ -1,36 +1,46 @@
-import { Component } from "./base/components"
-import { cloneTemplate, ensureElement } from "../utils/utils";
+import { Component } from './base/components';
+import { cloneTemplate, ensureElement } from '../utils/utils';
+import { IEvents } from './base/events';
 
 export interface IBasket {
-  basketItems: HTMLElement[];
-} 
+	basketItems: HTMLElement[];
+}
 
 export class Basket extends Component<IBasket> implements IBasket {
-  protected content: HTMLElement;
-  protected basketList: HTMLElement;
-  protected basketBody: HTMLElement;
+	protected content: HTMLElement;
+	protected basketList: HTMLElement;
+	protected basketBody: HTMLElement;
+	protected basketButton: HTMLButtonElement;
 
-  constructor(container: HTMLElement, content: HTMLElement) {
-    super(container)
-    this.content = content;
+	constructor(container: HTMLElement, content: HTMLElement, events: IEvents) {
+		super(container);
+		this.content = content;
 
-    this.basketBody = cloneTemplate('#basket');
-    this.basketList = ensureElement('.basket__list', this.basketBody);
+		this.basketBody = cloneTemplate('#basket');
+		this.basketList = ensureElement('.basket__list', this.basketBody);
+		this.basketButton = ensureElement<HTMLButtonElement>(
+			'.basket__button',
+			this.basketBody
+		);
 
-    let closeButton = ensureElement('.modal__close',this.container);
-    closeButton.addEventListener('click',()=>{
-      this.sv(false);
-    })
-  }
+		let closeButton = ensureElement('.modal__close', this.container);
+		closeButton.addEventListener('click', () => {
+			this.sv(false);
+		});
 
-  set basketItems(items: HTMLElement[]) {
-    this.basketList.replaceChildren(... items);
-  }
+		this.basketButton.addEventListener('click', () =>
+			events.emit('click: basket_button')
+		);
+	}
 
-  sv(v: boolean) {
-    if (v) {
-      this.content.replaceChildren(this.basketBody);
-    }
-    this.toggleClass(this.container, "modal_active", v);
-  }
+	set basketItems(items: HTMLElement[]) {
+		this.basketList.replaceChildren(...items);
+	}
+
+	sv(v: boolean) {
+		if (v) {
+			this.content.replaceChildren(this.basketBody);
+		}
+		this.toggleClass(this.container, 'modal_active', v);
+	}
 }
