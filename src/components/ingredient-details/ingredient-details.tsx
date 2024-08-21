0e72a7294f75624ff73e1ed1221af26119a030ab
Alina <alinbrode@yandex.ru>
2024-08-21 15:56:16 +0300

    direct transition to ingredient

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,13 +1,27 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { Preloader } from '../ui/preloader';
 import { IngredientDetailsUI } from '../ui/ingredient-details';
 import { useParams } from 'react-router-dom';
-import { RootState, useSelector } from '../../services/store';
+import { RootState, useDispatch, useSelector } from '../../services/store';
+import { fetchIngredients } from '../../services/slices/ingredient-slice';
+import { TIngredient } from '@utils-types';
 
 export const IngredientDetails: FC = () => {
   const { ingredients } = useSelector((state: RootState) => state.ingredients);
+  const [ingredientData, setIngredentData] = useState<TIngredient | null>(null);
+
   const params = useParams();
-  const ingredientData = ingredients.filter((item) => item._id == params.id)[0];
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!ingredients || !ingredients.length) {
+      dispatch(fetchIngredients());
+    }
+  }, []);
+
+  useEffect(() => {
+    setIngredentData(ingredients.filter((item) => item._id == params.id)[0]);
+  }, [ingredients]);
 
   if (!ingredientData) {
     return <Preloader />;
